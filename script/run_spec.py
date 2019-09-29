3e50963ca5bfcd5ed7667b4f2f68e1f4ce7c8ab1
Xiaozhu Meng <mxz297@gmail.com>
2019-09-28 21:58:42 -0500

    1. Temporarily remove 602.gcc_s from testing. 602.gcc_s will cause the cfi program to run for a long time and use lots of memory. It does not finish within 1 hour and used more than 20G of memory
    2. Paralleize the tests

--- script/run_spec.py
+++ script/run_spec.py
@@ -13,7 +13,7 @@ config_list = [ ("GCC-6.4.0", "gcc-test-liteCFI-single-thread-gcc-6.4.0.cfg"), \
 
 # The list of benchmark to run
 tests_list = ["600.perlbench_s", \
-        "602.gcc_s", \
+#        "602.gcc_s", \
         "605.mcf_s", \
         "620.omnetpp_s", \
         "623.xalancbmk_s", \
@@ -22,6 +22,8 @@ tests_list = ["600.perlbench_s", \
         "641.leela_s", \
         "648.exchange2_s", \
         "657.xz_s"]
+
+PARALLEL = 5 
 
 ###### END OF EDIT ############
 
@@ -39,33 +41,42 @@ def Run(config, test):
     cmd = "source shrc && "
     cmd += "runcpu --config={0} {1}".format(config, test)
     p = Popen(cmd, stdout=PIPE, stderr=PIPE,  shell=True, cwd=SPEC_ROOT, executable='/bin/bash')
-    msg, err = p.communicate()
-    if Failed(msg + err):
-        return "Fail"
-    elif Passed(msg + err):
-        return "Pass"
-    else:
-        return "Unknown"
+    return p, config, test
 
 def CleanOldDirs():
     cmd = "rm -rf benchspec/C*/*/run; "
     cmd += "rm -rf benchspec/C*/*/build; "
     cmd += "rm -rf benchspec/C*/*/exe "
-    print cmd
     p = Popen(cmd, stdout=PIPE, stderr=PIPE,  shell=True, cwd=SPEC_ROOT, executable='/bin/bash')
     msg, err = p.communicate()
     if len(err) > 0:
         print err
 
+def WaitForResults(runs, results):
+    for p, config, test in runs:
+        msg, err = p.communicate()
+        if Failed(msg + err):
+            ret = "Fail"
+        elif Passed(msg + err):
+            ret = "Pass"
+        else:
+            ret = "Unknown"
+        results[compiler][test] = ret
+        print compiler, test, ret
 
 results = {}
 for compiler, config in config_list:
     CleanOldDirs()
     results[compiler] = {}
+    runs = []
     for test in tests_list:
-        ret = Run(config, test)
-        results[compiler][test] = ret
-        print compiler, test, ret
+        runs.append( Run(config, test) )
+        if len(runs) == PARALLEL:
+            WaitForResults(runs, results)
+            runs = []
+    if len(runs) > 0:
+        WaitForResults(runs, results)
+
 
 line = "| SPEC "
 for compiler, config in config_list:
