1eab333bdbcb856c5da222f3886d62c17399dbd4
Xiaozhu Meng <mxz297@gmail.com>
2019-10-06 10:36:24 -0500

    Update spec regression script

--- script/run_spec.py
+++ script/run_spec.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from subprocess import *
 
 ##### EDIT #############
@@ -7,21 +8,34 @@ from subprocess import *
 SPEC_ROOT = "/home/xm13/projects/spec_cpu2017"
 
 # One compiler per configuration file
-config_list = [ ("GCC-6.4.0", "gcc-test-liteCFI-single-thread-gcc-6.4.0.cfg"), \
-        ("GCC-7.3.0", "gcc-test-liteCFI-single-thread-gcc-7.3.0.cfg"), \
-        ("GCC-8.3.0", "gcc-test-liteCFI-single-thread-gcc-8.3.0.cfg") ]
+config_list = [ ("GCC-6.4.0", "single-thread-gcc-6.4.0", "gcc-6.4.0-gcc-4.8.5-pcrkwvm"), \
+        ("GCC-7.4.0", "single-thread-gcc-7.4.0", "gcc-7.4.0-gcc-6.4.0-ja3xbno"), \
+        ("GCC-8.3.0", "single-thread-gcc-8.3.0", "gcc-8.3.0-gcc-7.3.0-qowc552") ]
 
 # The list of benchmark to run
 tests_list = ["600.perlbench_s", \
-#        "602.gcc_s", \
+        "602.gcc_s", \
+        "603.bwaves_s", \
         "605.mcf_s", \
+        "607.cactuBSSN_s", \
+        "619.lbm_s", \
         "620.omnetpp_s", \
+        "621.wrf_s", \
         "623.xalancbmk_s", \
         "625.x264_s", \
+#        "627.cam4_s", \
+        "628.pop2_s", \
         "631.deepsjeng_s", \
+        "638.imagick_s", \
         "641.leela_s", \
+        "644.nab_s", \
         "648.exchange2_s", \
+        "649.fotonik3d_s", \
+        "654.roms_s", \
         "657.xz_s"]
+
+#test_mode = ["light", "full"]
+test_mode = ["full"]
 
 PARALLEL = 5 
 
@@ -37,11 +51,13 @@ def Passed(output):
         return True
     return False
 
-def Run(config, test):
+def Run(mode, compiler, config, test, module):
     cmd = "source shrc && "
-    cmd += "runcpu --config={0} {1}".format(config, test)
+    cmd += "module unload gcc-6.4.0-gcc-4.8.5-pcrkwvm && "
+    cmd += "module load {0} && ".format(module)
+    cmd += "runcpu --config={0}-{2}.cfg {1}".format(config, test, mode)
     p = Popen(cmd, stdout=PIPE, stderr=PIPE,  shell=True, cwd=SPEC_ROOT, executable='/bin/bash')
-    return p, config, test
+    return p, mode, compiler, config, test
 
 def CleanOldDirs():
     cmd = "rm -rf benchspec/C*/*/run; "
@@ -52,49 +68,72 @@ def CleanOldDirs():
     if len(err) > 0:
         print err
 
-def WaitForResults(runs, results):
-    for p, config, test in runs:
-        msg, err = p.communicate()
-        if Failed(msg + err):
-            ret = "Fail"
-        elif Passed(msg + err):
-            ret = "Pass"
-        else:
-            ret = "Unknown"
-        results[compiler][test] = ret
-        print compiler, test, ret
+def GenerateTestConfigurations():
+    tests = []
+    for mode in test_mode:
+        for compiler, config, module in config_list:
+            for bench in tests_list:
+                tests.append ( (mode, compiler, config, bench, module) )
+    return tests
 
-results = {}
-for compiler, config in config_list:
-    CleanOldDirs()
-    results[compiler] = {}
-    runs = []
-    for test in tests_list:
-        runs.append( Run(config, test) )
-        if len(runs) == PARALLEL:
-            WaitForResults(runs, results)
-            runs = []
-    if len(runs) > 0:
-        WaitForResults(runs, results)
+def WaitForFinish(results, runs):
+    while True:
+        for i in range(len(runs)):
+            p = runs[i][0]
+            if p.poll() is not None:
+                # This test is done
+                out = p.stdout.read() + p.stderr.read()
+                if Failed(out):
+                    ret = "Fail"
+                elif Passed(out):
+                    ret = "Pass"
+                else:
+                    ret = "Unknown"
+                mode = runs[i][1]
+                compiler = runs[i][2]
+                bench = runs[i][4]
+                results[ (mode, compiler, bench) ] = ret
+                print mode, compiler, bench, ret
+                del runs[i]
+                return
+        # All tests are running
+        # Wait for some time and re-check
+        time.sleep(30)
 
-
-line = "| SPEC "
-for compiler, config in config_list:
-    line += " | {0}".format(compiler)
-line += " | "
-print line
-line = "| --- | "
-for compiler, config in config_list:
-    line += " --- | "
-print line
-
-for test in tests_list:
-    line = "| {0}".format(test)
-    for compiler, config in config_list:
-        line += " | {0}".format(results[compiler][test])
-    line += " |"
+def PrintResultsTable(mode):
+    print mode 
+    line = "| SPEC "
+    for compiler, config, _ in config_list:
+        line += " | {0}".format(compiler)
+    line += " | "
+    print line
+    line = "| --- | "
+    for compiler, config, _ in config_list:
+        line += " --- | "
     print line
 
+    for test in tests_list:
+        line = "| {0}".format(test)
+        for compiler, config, _ in config_list:
+            line += " | {0}".format(results[(mode, compiler, test)])
+        line += " |"
+        print line
+        
+        
+CleanOldDirs()
+configs = GenerateTestConfigurations()
 
+results = {}
+runs = []
+for mode, compiler, cfg_prefix, bench, module in configs:
+    runs.append( Run(mode, compiler, cfg_prefix, bench, module) )
+    if len(runs) == PARALLEL:
+        WaitForFinish(results, runs)
+
+while len(runs) > 0:
+    WaitForFinish(results, runs)
+
+#PrintResultsTable("light")
+PrintResultsTable("full")
 
 
