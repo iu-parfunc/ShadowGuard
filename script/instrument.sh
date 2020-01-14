dc14804dd1e90d275b14f9405e01383a4314bd6b
Buddhika <chamibuddhika@gmail.com>
2020-01-14 14:08:41 -0500

    Update instrument script to output the instrumented binary with the same name.

--- script/instrument.sh
+++ script/instrument.sh
@@ -2,8 +2,10 @@
 
 bench=$1
 
-dyninst_lib=/home/buddhika/Builds/retguard/thirdparty/dyninst-10.1.0/install/lib
+root=$LITECFI_HOME
+
+dyninst_lib=$root/thirdparty/dyninst-10.1.0/install/lib
 
 cp $bench $bench.orig
 
-OMP_NUM_THREADS=1 LD_LIBRARY_PATH=$dyninst_lib:$LD_LIBRARY_PATH DYNINSTAPI_RT_LIB=$dyninst_lib/libdyninstAPI_RT.so /home/buddhika/Builds/retguard/bazel-bin/src/cfi --shadow_stack=light --stats=ANALYSIS.txt --vv --output=$bench $bench
+OMP_NUM_THREADS=1 LD_LIBRARY_PATH=$dyninst_lib:$LD_LIBRARY_PATH DYNINSTAPI_RT_LIB=$dyninst_lib/libdyninstAPI_RT.so $root/bazel-bin/src/cfi --shadow_stack=light --stats=ANALYSIS.txt --vv --output=$bench $bench
